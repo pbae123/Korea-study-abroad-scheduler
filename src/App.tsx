83aca17a8b5b5cc92a9fe2e36a4532f8f79d497e
pbae123 <pbae@nd.edu>
2026-07-12 23:48:44 -0400

    add: vite react-ts scaffold with tailwind, dnd-kit, react-hook-form, headlessui

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <h1 className="p-4 text-lg font-semibold">Korea Study Abroad Scheduler</h1>
+    </div>
+  )
+}
+
+export default App
